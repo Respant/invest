353a335489cf58339d3b11899e85be89643f1950
Anton Hobzhelian <anton.hobzhelian@mensemedia.net>
2016-12-03 21:44:57 +0200

    new bundle

---
+++ src/Respant/InvestBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Respant\InvestBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('RespantInvestBundle:Default:index.html.twig');
+    }
+}
---
+++ src/Respant/InvestBundle/RespantInvestBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Respant\InvestBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RespantInvestBundle extends Bundle
+{
+}
