bb5aac818af4f164ec5e9a4c94ceffa9507c7869
Anton Hobzhelian <anton.hobzhelian@mensemedia.net>
2016-12-03 22:10:27 +0200

    generate new entity Company

---
+++ src/Respant/InvestBundle/Entity/Company.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace Respant\InvestBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Company
+ *
+ * @ORM\Table(name="company")
+ * @ORM\Entity(repositoryClass="Respant\InvestBundle\Repository\CompanyRepository")
+ */
+class Company
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255, unique=true)
+     */
+    private $name;
+
+    /**
+     * @var \stdClass
+     *
+     * @ORM\Column(name="type", type="object")
+     */
+    private $type;
+
+    /**
+     * @var array
+     *
+     * @ORM\Column(name="currency", type="simple_array")
+     */
+    private $currency;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="startDate", type="datetime")
+     */
+    private $startDate;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Company
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set type
+     *
+     * @param \stdClass $type
+     *
+     * @return Company
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    /**
+     * Get type
+     *
+     * @return \stdClass
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set currency
+     *
+     * @param array $currency
+     *
+     * @return Company
+     */
+    public function setCurrency($currency)
+    {
+        $this->currency = $currency;
+
+        return $this;
+    }
+
+    /**
+     * Get currency
+     *
+     * @return array
+     */
+    public function getCurrency()
+    {
+        return $this->currency;
+    }
+
+    /**
+     * Set startDate
+     *
+     * @param \DateTime $startDate
+     *
+     * @return Company
+     */
+    public function setStartDate($startDate)
+    {
+        $this->startDate = $startDate;
+
+        return $this;
+    }
+
+    /**
+     * Get startDate
+     *
+     * @return \DateTime
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+}
+
